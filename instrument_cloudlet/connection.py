8ab2b2501ee6efe523cc19b5ef2dbf799cd09a95
Austin <93621884+austinmclain@users.noreply.github.com>
2023-04-27 19:41:13 -0500

    Add ability to download image via SSH

--- instrument_cloudlet/connection.py
+++ instrument_cloudlet/connection.py
@@ -1,7 +1,8 @@
 from paramiko import SSHClient, AutoAddPolicy
 import requests
+import os
 
-def ssh_connection(cmd):
+def ssh_connection(cmd, goal='other'):
     try:
         client = SSHClient()
         client.set_missing_host_key_policy(AutoAddPolicy())
@@ -9,21 +10,31 @@ def ssh_connection(cmd):
 
         # Configuration specific to EC2 instance acting as SEM
         client.connect('3.80.102.76', username='ec2-user',
-                       key_filename= "/Users/amac/Desktop/rise-project-key.pem")
-        
+                    key_filename= "/Users/amac/Desktop/rise-project-key.pem")
+
     except Exception as e:
         print(str(e))
         return{"status": 0, "msg": "Connection not established with server"}
+    
+    # If goal is to download...
+    if (goal == 'download'):
+        try:
+            sftp = client.open_sftp()
+            localpath = 'image_1'
+            remotepath = 'experiment_1/Misc_pollen.jpg'
+            sftp.get(remotepath, localpath)
+            sftp.close()
+            client.close()
+            return{"status": 1, "msg": "command ran successfully", "data": localpath}
+        except Exception as e:
+            print(str(e))
+            return{"status": 0, "msg": "Error msg " + str(e)}
 
     try:
         (stdin, stdout, stderr) = client.exec_command(cmd)
         cmd_output = stdout.read().decode()
         print('Output:\n', cmd_output)
         client.close()
-        # save = input('Would you like to save this image? (y/n): ')
-        # if (save == 'y'):
-        #     r = requests.get('http://localhost:8080')
-        #     print(r.content)
         return{"status": 1, "msg": "command ran successfully", "data": cmd_output}
 
     except Exception as e:
