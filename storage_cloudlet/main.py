003aad7aef6e6244b4cdbf0353136cb86e8ce78f
Austin <93621884+austinmclain@users.noreply.github.com>
2023-04-27 22:35:16 -0500

    Create endpoint to receive and store image

--- storage_cloudlet/main.py
+++ storage_cloudlet/main.py
@@ -1,14 +1,12 @@
-from flask import Flask
+from flask import Flask, request
+from StorageServices import StorageServices
 
 app = Flask(__name__)
 
-@app.route("/storageApi", methods=['GET'])
-def hello_world():
-    return "<p>Storage Cloudlet</p>\n"
-
-# @app.route("/save_image", methods=['POST'])
-#     Get image metadata from request
-#     Send to S3
+@app.route("/storageApi/receiveImage", methods=['POST'])
+def receiveImage():
+    services = StorageServices()
+    return services.storeImage('image_1.png', request.data)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8081, debug=True)
