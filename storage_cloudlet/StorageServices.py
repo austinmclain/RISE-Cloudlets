45f751e63cbddf6ec5d89a479760312a118ae8dc
Austin <93621884+austinmclain@users.noreply.github.com>
2023-04-27 22:35:31 -0500

    Create StorageServices.py

---
+++ storage_cloudlet/StorageServices.py
@@ -0,0 +1,20 @@
+import base64
+import boto3
+
+class StorageServices:
+    def __downloadImage(self, localPath, contents):
+        try:
+            new_image = open(localPath, 'wb')
+            new_image.write(base64.b64decode(contents))
+            new_image.close()
+        except Exception as e:
+            print(e)
+    
+    # Note: This requires IAM configuration
+    def storeImage(self, localPath, contents):
+        try:
+            self.__downloadImage(localPath, contents)
+            client = boto3.client('s3', region_name='us-east-1')
+            client.upload_file(localPath, 'rise-bucket123', localPath)
+        except Exception as e:
+            print(e)
