2cdb182435f17f53fdcb926a16f1ed08202d0101
Austin <93621884+austinmclain@users.noreply.github.com>
2023-04-27 20:29:24 -0500

    Create endpoint for downloading image

--- instrument_cloudlet/main.py
+++ instrument_cloudlet/main.py
@@ -3,10 +3,19 @@ from connection import ssh_connection
 
 app = Flask(__name__)
 
-@app.route("/cloudletApi", methods=['GET'])
-def cloudletApi():
+@app.route("/instrumentApi/command", methods=['GET'])
+def command():
     try:
-        return ssh_connection('ls')
+        ssh = ssh_connection()
+        return ssh.command('ls')
+    except Exception as e:
+        return e
+    
+@app.route("/instrumentApi/download", methods=['GET'])
+def download():
+    try:
+        ssh = ssh_connection()
+        return ssh.download('experiment_1/Misc_pollen.jpg')
     except Exception as e:
         return e
 
