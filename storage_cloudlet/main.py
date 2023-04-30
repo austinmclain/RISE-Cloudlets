cc8036422281561dba5e5efed98cf513755021c5
Austin <93621884+austinmclain@users.noreply.github.com>
2023-04-29 22:52:04 -0500

    Add exception handling to main.py

--- storage_cloudlet/main.py
+++ storage_cloudlet/main.py
@@ -1,12 +1,15 @@
 from flask import Flask, request
 from StorageServices import StorageServices
+import shortuuid
 
 app = Flask(__name__)
 
-@app.route("/storageApi/receiveImage", methods=['POST'])
-def receiveImage():
-    services = StorageServices()
-    return services.storeImage('image_1.png', request.data)
+@app.route("/storageApi/", methods=['POST'])
+def storeImage():
+    try:
+        return StorageServices.storeImage(f'1-${shortuuid.uuid()}.png', request.data)
+    except Exception as e:
+        return e
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8081, debug=True)
