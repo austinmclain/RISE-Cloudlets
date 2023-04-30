a3056460bcc1768bf6e9eec400aa57cf132691bd
Austin <93621884+austinmclain@users.noreply.github.com>
2023-04-30 17:37:17 -0500

    Fix bug in S3 put request

--- storage_cloudlet/StorageServices.py
+++ storage_cloudlet/StorageServices.py
@@ -7,7 +7,7 @@ class StorageServices:
         image = f'1-{uniqueId}.png'
         try:
             s3 = boto3.resource('s3')
-            s3.Object('rise-bucket123', image).put(Body=contents, Metadata={'instrument': 1, 'project': 1})
+            s3.Object('rise-bucket123', image).put(Body=contents, Metadata={'instrument': '1', 'project': '1'})
             return {"status": 1, "msg": "image stored successfully", "data": image}
         except Exception as e:
             return {"status": 0, "msg": "error msg " + str(e)}
