5dbfe07a092257e348d52443cffba0183f22052e
Austin <93621884+austinmclain@users.noreply.github.com>
2023-04-29 22:52:20 -0500

    Refactor StorageServices.py

--- storage_cloudlet/StorageServices.py
+++ storage_cloudlet/StorageServices.py
@@ -3,12 +3,9 @@ import boto3
 
 class StorageServices:
     def __downloadImage(self, localPath, contents):
-        try:
-            new_image = open(localPath, 'wb')
-            new_image.write(base64.b64decode(contents))
-            new_image.close()
-        except Exception as e:
-            print(e)
+        new_image = open(localPath, 'wb')
+        new_image.write(base64.b64decode(contents))
+        new_image.close()
     
     # Note: This requires IAM configuration
     def storeImage(self, localPath, contents):
@@ -16,6 +13,6 @@ class StorageServices:
             self.__downloadImage(localPath, contents)
             client = boto3.client('s3', region_name='us-east-1')
             client.upload_file(localPath, 'rise-bucket123', localPath)
-            return{"status": 'success'}
+            return {"status": 1, "msg": "image stored successfully", "data": localPath}
         except Exception as e:
-            print(e)
+            return {"status": 0, "msg": "error msg " + str(e)}
