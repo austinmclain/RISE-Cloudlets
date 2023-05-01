f1425c26ad17b620ee98ec8e8305372c3eb21955
Austin <93621884+austinmclain@users.noreply.github.com>
2023-04-30 19:32:44 -0500

    Encode metadata in prefix of image name

--- storage_cloudlet/StorageServices.py
+++ storage_cloudlet/StorageServices.py
@@ -4,10 +4,11 @@ import boto3
 class StorageServices:
     # Note: This requires IAM configuration
     def storeImage(self, uniqueId, contents):
-        image = f'1-{uniqueId}.png'
+        # Prefix is encoded metadata about origin of image (project and instrument)
+        image = f'prj-1_inst-1_{uniqueId}.png'
         try:
             s3 = boto3.resource('s3')
-            s3.Object('rise-bucket123', image).put(Body=contents, Metadata={'instrument': '1', 'project': '1'})
+            s3.Object('rise-bucket123', image).put(Body=contents, Metadata={'project': '1', 'instrument': '1'})
             return {"status": 1, "msg": "image stored successfully", "data": image}
         except Exception as e:
             return {"status": 0, "msg": "error msg " + str(e)}
