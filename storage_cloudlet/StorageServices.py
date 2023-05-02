423a5bf2b621f84eae5e1d555c167fd37f028cb8
Austin <93621884+austinmclain@users.noreply.github.com>
2023-05-01 20:51:00 -0500

    Fix missing decode

--- storage_cloudlet/StorageServices.py
+++ storage_cloudlet/StorageServices.py
@@ -8,7 +8,7 @@ class StorageServices:
         image = f'prj-1_inst-1_{uniqueId}.png'
         try:
             s3 = boto3.resource('s3')
-            s3.Object('rise-bucket123', image).put(Body=contents, Metadata={'project': '1', 'instrument': '1'})
+            s3.Object('rise-bucket123', image).put(Body=base64.b64decode(contents), Metadata={'project': '1', 'instrument': '1'})
             return {"status": 1, "msg": "image stored successfully", "data": image}
         except Exception as e:
             return {"status": 0, "msg": "error msg " + str(e)}
