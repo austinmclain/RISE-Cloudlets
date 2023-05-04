5a6fbc00694f63e64c7188e11a8d816fc4ce2af0
Austin <93621884+austinmclain@users.noreply.github.com>
2023-05-03 23:46:55 -0500

    Add getImage endpoint

--- storage_cloudlet/main.py
+++ storage_cloudlet/main.py
@@ -10,5 +10,12 @@ def storeImage():
     s3 = StorageServices()
     return s3.storeImage(uniqueId, request.data)
 
+@app.route("/storageApi/getImage", methods=['GET'])
+def getImage():
+    args = request.args
+    key = args.get("key")
+    return getImage(key)
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8081, debug=True)
