5bbbc6d60a576cc8c5992d3121788ef1c7b03bed
Austin <93621884+austinmclain@users.noreply.github.com>
2023-05-04 00:20:59 -0500

    Update port numbers

--- instrument_cloudlet/main.py
+++ instrument_cloudlet/main.py
@@ -22,4 +22,4 @@ def saveImage():
         return e
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- storage_cloudlet/main.py
+++ storage_cloudlet/main.py
@@ -11,4 +11,4 @@ def storeImage():
     return s3.storeImage(uniqueId, request.data)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8081, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
